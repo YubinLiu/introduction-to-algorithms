4b02fbaaebc05e75f37ec7c687f4e089e095f491
Liuyubin <lybin0904@gmail.com>
2017-08-24 23:18:36 +0800

    add dynamic programming README

--- 15_动态规划/例子/code/FirstTest.java
+++ 15_动态规划/例子/code/FirstTest.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 
 public class FirstTest {
-    
+
     public static int[][] triangle = new int[100][100];
     public static int n = 0;
+
     public static void main(String[] args) {
-        
+
         int i;
         int j;
         Scanner in = new Scanner(System.in);
@@ -15,20 +16,20 @@ public class FirstTest {
                 triangle[i][j] = in.nextInt();
             }
         }
-        
+
         System.out.println(maxSum(1, 1));
         in.close();
     }
-    
+
     public static int maxSum(int i, int j) {
         if (i == n) return triangle[i][j];
-        
+
         int x = maxSum(i + 1, j);
         int y = maxSum(i + 1, j + 1);
-        
+
         return max(x, y) + triangle[i][j];
     }
-    
+
     public static int max(int m, int n) {
         return m > n ? m : n;
     }
